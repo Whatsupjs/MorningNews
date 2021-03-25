f4951ead36cd2fd18957efa291ea826a4c772a1f
whatsupjs <you@example.com>
2021-03-24 23:08:14 -0400

    fixed crash on render

--- src/Covid.js
+++ src/Covid.js
@@ -1,21 +1,29 @@
 import React, { useState, useEffect } from 'react';
 //this api returns a summary array obj. 
-const baseurl="https://api.opencovid.ca/summary";
+const baseurl = "https://api.opencovid.ca/summary";
 
 function Covid() {
-    
+
     const [covid, setCovid] = useState({});
-    
+    const date = new Date().toLocaleString();
     //similar to componentDidMount for functional hooks 
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`${baseurl}?loc=ON`)
-        .then(res => res.json())
-        .then(result => setCovid(result));
+            .then(res => res.json())
+            .then(result => setCovid(result));
     }, []);
-    
+
     return (
-        <div>
-            <p>Current active cases: {covid.summary[0].active_cases}</p>
+        <div className="covid-container">
+            
+            <div>Last updated: {date} </div>
+            {(typeof covid.summary != "undefined") ? (
+                <div>
+                    <div>
+                        <p>Current cases: {covid.summary[0].cases}</p>
+                    </div>
+                </div>
+            ) : ('')}
         </div>
     )
 }
